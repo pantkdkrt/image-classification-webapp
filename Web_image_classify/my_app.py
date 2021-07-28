814f09fca1b4f587e65216d44eccea52f8f82586
pantkdkrt <conan656415@gmail.com>
2021-07-28 13:18:47 +0700

    add files

---
+++ Web_image_classify/my_app.py
@@ -0,0 +1,66 @@
+from flask import Flask, request, render_template
+import tensorflow
+from tensorflow.keras.models import load_model
+from skimage.transform import resize
+import matplotlib.pyplot as plt
+import os
+import numpy as np
+from werkzeug.utils import secure_filename
+
+UPLOAD_FOLDER = './UPLOAD_FOLDER'
+
+ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
+
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+model = load_model('model.h5')
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+# def upload_file(file):
+#         if file and allowed_file(file.filename):
+#             filename = secure_filename('image')
+#             file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+
+
+def resize_img(image):
+    img = plt.imread(image)
+    resize_image = resize(img, (32,32,3))
+    return resize_image
+
+@app.route('/',methods=['GET'])
+def show_predict_page():
+    return render_template('home.html')
+
+@app.route('/',methods=['POST'])
+def show_predict_result():
+    if request.files:
+        image = request.files["fileUpload"]
+        #filename = secure_filename(image.filename)
+        image.save(os.path.join(app.config['UPLOAD_FOLDER'], 'image.jpg'))
+        resize_i = resize_img(image)
+        predictions = model.predict(np.array([resize_i]))
+        list_index = [0,1,2,3,4,5,6,7,8,9]
+        x = predictions
+
+        for i in range(10):
+            for j in range(10):
+                if x[0][list_index[i]] > x[0][list_index[j]]:
+                    temp = list_index[i]
+                    list_index[i] = list_index[j]
+                    list_index[j] = temp
+        
+        classification = ['airplane', 'automobile', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck']
+        result = classification[list_index[0]]
+    # for i in range(len(classification)):
+    #     result.append(classification[list_index[i]])
+    #     print(result)
+    # result = result[0]
+    return render_template('home.html',result=result)   
+
+
+#def print_date_time():
+#    print(time.strftime("%A,%d.%B %Y %I:%M:%S %p"))
+if __name__ == '__main__':
+    app.run()
